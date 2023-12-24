8bcd739798031de3a16d3947c2fbb6f541b2c98d
Ydupc <65360930+CyberDudeJ@users.noreply.github.com>
2023-12-24 19:06:56 +0000

    Rewrite gpm + others

--- gpm.py
+++ gpm.py
@@ -1,86 +1,57 @@
-#!/usr/bin/env python3
-# GPM/ Git Package Manager, by Jay Salway 20/12/2023
-import os, re, json, requests, platform
+#!/usr/bin/env python
 
-def exec_cmd(command):
-    stream = os.popen(command)
-    output = stream.read()
-    output
+import sys, os, re, json, requests, platform
 
-    return output
+# Global Variables
+arg1 = None
+arg2 = None
 
-def exec_download_git(repo):
-    command = "git clone " + repo
-    stream = os.popen(command)
-    output = stream.read()
-    output
-    
-    return output
+def main():
+    global arg1, arg2 # Decleare variables as global (again)
 
-def exec_remove_wd(directory):
-    command = "rm -rf " + directory
-    stream = os.popen(command)
-    output = stream.read()
-    output
+    # Check if the correct number of arguments has been provided
+    if len(sys.argv) < 2:
+        print("[WARN] Usage: {} [arg1] [arg2]".format(sys.argv[0]))
+        sys.exit(1)
 
-    return output
+    # Extract command-line arguments
+    arg1 = sys.argv[1] if len(sys.argv) > 1 else None
 
-def exec_install_deps(deps):
-    osname = os.name()
-    if osname == 'debain' or 'Debian' or 'ubuntu' or 'Ubuntu':
-        command = "apt-get install" + deps + "-y"
-        stream = os.popen(command)
-        output = stream.read()
-        output
+    # Check the value of arg1 and adjust argument parsing
+    if arg1 in ["h", "help"]:
+        print("[INFO] See https://github.com/CyberDudeJ/gpm for more information")
+    elif arg1 in ["i", "install", "get"]:
+        # Check if arg2 is provided
+        if len(sys.argv) > 2:
+               arg2 = sys.argv[2]
+               # If arg2 is provided, load package list json file
+               jsonurl = "https://raw.githubusercontent.com/CyberDudeJ/gpm/1.2/packagelist.json"
+               resp = requests.get(jsonurl)
+               data = json.loads(resp.text)
+               #print(data) # debug
+               for i in data:
+                   # if arg2 = package name
+                   if arg2 == i['Name']:
+                       # check if local or external install
+                       if i['InstallType'] == "Local":
+                           # Init local install
+                           print("local")
+                       elif i['InstallType'] == "External":
+                           # Init external install using package manager which was found earlier
+                           print("external")
+        else:
+               # If arg2 is not provided, end script
+               print(f"Arg2 is required for {arg1} to function correctly.")
+               sys.exit(1)
+    else:
+        print("Invalid cmd")
 
-    return output
-
-def running_as_root() -> bool:
-    return os.getuid() == 0
-
+# Check what platform gpm is being run on
 platform = platform.system()
 
 if platform == 'Linux':
-    print("[INFO] Reading package list.")
-    url = "https://raw.githubusercontent.com/CyberDudeJ/gpm/main/packagelist.json"
-    resp = requests.get(url)
-    data = json.loads(resp.text)
-    print("[INFO] Loaded package list.")
-    if running_as_root() == True:
-        searchPackage = input("Package: ")
-        for i in data:
-            if searchPackage == i['Name']:
-                acceptdeny = input("You are attempting to install the selected package. Would you like to continue? [y/n] ")
-                if acceptdeny == "y":
-                    if i['WorkingDirectory'] == "None":
-                        print("[INFO] Installing via external package manager or script.")
-                        exec_cmd(i['RunScript'])
-                        print("[INFO] GPM has finished. Exiting Application")
-                    else:
-                        if i['Deps'] == "None":
-                           exec_download_git(i['Repo'])
-                           exec_cmd(i['RunScript'])
-                           exec_remove_wd(i['WorkingDirectory'])
-                           print("[INFO] GPM has finished. Exiting Application")
-                        else:
-                            print("[INFO] Installing dependencies.") 
-                            exec_install_deps(i['Deps'])
-                            print("[INFO] Finished installing dependencies.")
-                            exec_download_git(i['Repo'])
-                            exec_cmd(i['RunScript'])
-                            exec_remove_wd(i['WorkingDirectory'])
-                            print("[INFO] GPM has finished. Exiting Application")
-                elif acceptdeny == "n":
-                    print("Package installation aborted.")
-                else:
-                    print("Package installation aborted. Please select a valid option.")
-    else:
-        print("This package manager must be run as root.")
-elif platform == 'Windows':
-    print("[WARN] GPM cannot be run on Windows, or Windows-based systems.")
-elif platform == 'Darwin':
-    print("[WARN] GPM cannot be run on MacOS, or Darwin-based systems.")
-elif plaform == "Linux2":
-    print("[WARN] GPM cannot be run on this version of Linux. Please update your system.")
+    main()
 else:
-    print("[WARN] GPM cannot determine this system's platform, therefore GPM cannot be run.")
+    print("[WARN] GPM must be run on a linux-based platform. Exiting application")
+    sys.exit(1)
+
